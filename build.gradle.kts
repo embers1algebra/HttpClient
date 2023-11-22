3240304b837725ff8454c459f7db9a08cad042a8
Di Giovanni Raffaele <151665591+embers1algebra@users.noreply.github.com>
2023-11-22 20:55:56 +0300

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    implementation("io.ktor:ktor-client-core:1.7.4")
+    implementation("io.ktor:ktor-client-cio:1.7.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+}
